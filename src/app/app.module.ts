df1a9c19e34aedc3c34e432055a7fe4900d56580
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-10-24 16:42:55 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+/*
+  Angular module help us organize our application and
+  resolves the template (view) resolution environment
+  by declaring the app.component so that the compiler
+  can find it
+*/
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+// We identify the class as Angular module by attaching the @NgModule decorator
+//The metadata for AppModule class are: declarations, imports, and bootstrap
+//properties of the @NgModule decorator
+export class AppModule { }

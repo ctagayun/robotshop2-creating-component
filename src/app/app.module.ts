d8b0d20ae31860c08d8a9e275da5243204d61679
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-10-25 08:21:14 -0500

    Completed Module 3 Creating Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 /*
   Angular module help us organize our application and
@@ -14,7 +16,8 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
-
+    CatalogComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule
